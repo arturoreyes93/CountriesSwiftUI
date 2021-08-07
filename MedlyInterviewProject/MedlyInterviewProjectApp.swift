ca8e1a8885f1d014c72c9d94ac1362c5222498c6
Arturo <Reyes>
2021-08-07 12:05:02 -0600

    Initial Commit

---
+++ MedlyInterviewProject/MedlyInterviewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedlyInterviewProjectApp.swift
+//  MedlyInterviewProject
+//
+//  Created by Arturo Reyes on 8/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MedlyInterviewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
